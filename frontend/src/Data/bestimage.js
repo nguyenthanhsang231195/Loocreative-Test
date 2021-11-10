b8e26e883d839b300bb3769b3bfb9dbfbec24ae1
unknown <you@example.com>
2021-11-10 21:09:36 +0700

    setup

---
+++ frontend/src/Data/bestimage.js
@@ -0,0 +1,22 @@
+const best = [
+    {
+        name: "How to create mobie-optimized",
+        image_main: '/images/best-image-01@3x.png',
+        image_top: '/images/producticon-01@3x.png',
+        image_go: '/images/go-icon@3x.png',
+    },
+    {
+        name: "How to create mobie-optimized",
+        image_main: '/images/best-image-02@3x.png',
+        image_top: '/images/producticon-02@3x.png',
+        image_go: '/images/go-icon@3x.png',
+    },
+    {
+        name: "How to create mobie-optimized",
+        image_main: '/images/best-image-03@3x.png',
+        image_top: '/images/producticon-03@3x.png',
+        image_go: '/images/go-icon@3x.png',
+    } 
+]
+
+export default best; 
