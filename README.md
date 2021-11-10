825d16ecf9d7ebf2f2a18c8a703a0b8d9e27a5c8
unknown <you@example.com>
2021-11-10 14:40:54 +0700

    ReactJs and Github

---
+++ README.md
@@ -0,0 +1,3 @@
+I . ReactJs and Github
+1) npx create-react-app frontend
+2) Github : https://github.com/nguyenthanhsang231195/Loocreative-Test.git
---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+    return (
+<div className="App">
+    <header className="header-website">
+        Header
+    </header> 
+
+    <main className="main-website">
+        Main
+    </main>
+
+    <footer className="footer-website">
+        Footer
+    </footer>
+</div>
+    );
+}
+
+export default App;
